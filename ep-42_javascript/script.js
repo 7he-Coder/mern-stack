dd3b308c76c28e31eac94bd9dc4fb732e374ebdd
Abhay_Gautam <150480377+7he-Coder@users.noreply.github.com>
2025-03-17 15:50:43 +0000

    js by love babbar

--- ep-42_javascript/script.js
+++ ep-42_javascript/script.js
@@ -1,5 +1,72 @@
 // Loops & Strings 👍
 
-for (let i = 1; i >= 10; i++) {
-  console.log(i);
-}
+// for (let i = 1; i <= 5; i++) {
+//   console.log(i);
+// }
+// for (let i = 5; i >= 1; i--) {
+//   console.log(i);
+// }
+
+// break 👍
+// for (let i = 1; i <= 6; i++) {
+//   if (i == 4) {
+//     break;
+//   }
+//   else {
+//     console.log(i);
+
+//   }
+// }
+
+// continue 👍
+// for (let i = 1; i <= 4; i++) {
+//   if (i == 3) {
+//     continue;
+//   }
+//   else {
+//     console.log(i);
+//   }
+// }
+
+
+// let i = 1;
+// while (i <= 5) {
+//   console.log(i);
+//   i++;
+// }
+
+// let i = 1;
+// while (i < 5) {
+//   console.log("Inside the loop");
+//   if (i == 3) {
+//     i++;
+//     continue;
+//   }
+//   else {
+//     console.log(i);
+//     i++;
+//   }
+// }
+
+// let i = 1;
+// do {
+//   console.log('babbar');
+//   //updation
+//   i++;
+// } while (i < 10);
+
+// let i = 1;
+// do {
+//   console.log(i);
+//   //updation
+//   i++;
+// } while (i <= 10);
+
+// strings 👍 
+// let firstName = "Love";
+// let lastName = 'Babbar';
+// let name = `Love 
+// Babbar`
+
+// let myName = new String(`Love Babbar`);
+// console.log(myName)
