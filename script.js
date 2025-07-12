920930451a5f265bafdc2fd2ee4b983c3f155d15
Abhay_Gautam <150480377+7he-Coder@users.noreply.github.com>
2025-07-12 07:04:25 +0000

    js

---
+++ script.js
@@ -0,0 +1,12 @@
+// Universal 👻
+console.log(`Hello, World!`);
+
+// if...else 
+// let tomorrow = `rain`;
+// if (tomorrow == `hazzy`) {
+//   console.log(`need raincoat`)
+// } else {
+//   console.log(`no need to take rain coat`)
+// }
+
+// 👻 Write a program that works out whether if a given year is a leap year or not ? | Hit : https://www.mathsisfun.com/leap-years.html...
